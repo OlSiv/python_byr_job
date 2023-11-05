acc9302b78ef1ac9dd2f74ba7ffbe84c4b857152
Oleg Siv <pomo24abc@gmail.com>
2023-11-05 19:52:17 +0300

    added_robot_name_3

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -3,14 +3,22 @@ import random
 
 
 class Robot:
+    
     def __init__(self):
-        self.name = (''.join(random.choice(string.ascii_uppercase) for _ in range(
-            2))) + (''.join(random.choice(string.digits) for _ in range(3)))
+        self.name = self.gener()
 
     def reset(self):
         while True:
-            n_name = (''.join(random.choice(string.ascii_uppercase) for _ in range(
-                2))) + (''.join(random.choice(string.digits) for _ in range(3)))
+            n_name = self.gener()
             if n_name != self.name:
                 break
         self.name = n_name
+
+    def gener(self):
+        result = ''
+        for _ in range (2):
+            result += random.choice(string.ascii_uppercase)
+        for _ in range (3):
+            result += random.choice(string.digits)
+        return result 
+    
